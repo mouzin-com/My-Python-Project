c9cf0f1d930e40b077200bc213ac246d36d9da65
mouzin-cloud <khaled.hamdy@mouzin.com>
2022-01-10 13:41:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+CalculationToUnites= 24
+NameofUnit="Hours"
+
+def days_of_units(num_of_days,custom_message):
+    print()
+    print(f"The Total Number of units = {num_of_days} and {custom_message} and {CalculationToUnites} also {NameofUnit}")
+    print("All Good")
+
+days_of_units(f"xxx {20}{3.4*4}","Hello Wolrd!!!")
+
